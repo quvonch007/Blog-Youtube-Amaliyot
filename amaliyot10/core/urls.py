ec7a0658c76483040cced17b6f9f16255faa2cc3
quvonch007 <pasquuv@gmail.com>
2026-06-12 10:22:08 +0500

    AmaliyotRestFramework

---
+++ amaliyot10/core/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for core project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+urlpatterns = [
+    #Django yuqoridan pastga o'qigani uchun sxemani yuqoriga ko'chirdik
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="schema_redoc"),
+    path("api/v1/schema/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="schema_swagger"),
+
+    path('admin/', admin.site.urls),
+    path('api/v1/', include("api.urls")),
+    path("api/v1/auth/", include("rest_framework.urls")),
+    path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
+    path("api/v1/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("", include("posts.urls")),
+]
---
+++ amaliyot10/posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+
+app_name = 'posts'
+urlpatterns = [
+    path("", PostListView.as_view(), name='list'),
+    path("create/", PostCreateView.as_view(), name="create"),
+    path("<int:pk>/update/", PostUpdateView.as_view(), name="update"),
+    path("<int:pk>/delete/", PostDeleteView.as_view(), name="delete"),
+    path("<int:pk>/", PostDetailView.as_view(), name='detail'),
+]
